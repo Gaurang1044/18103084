e31d8d7ba488e681b3a30f9731df20fa5c67960d
Gaurang Gupta <Gaurang1044>
2020-09-05 03:42:46 +0530

    Added Assignment 2

---
+++ Assignment2/Question1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        String first = in.nextLine();
+        String second = in.nextLine();
+    }
+}
